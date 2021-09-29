2c79ff348061f3d85c905d7465aa64fe4215ce8c
sabir balouch <p180085@nu.edu.pk>
2021-09-29 23:38:28 +0500

    Add files via upload

---
+++ a04.py
@@ -0,0 +1,31 @@
+## IMPORTS GO HERE if required
+
+## END OF IMPORTS 
+
+
+#### YOUR CODE FOR numbersToText function, GOES HERE ####
+
+
+
+
+
+
+#### End OF MARKER
+
+
+dic_numbers={}
+
+
+#Open the file and convert each line into a python dictionary using loop 
+	#a. strip and then split via fullstop (.) each line and store it in a variable t. 
+	#b. Now split t[1] via comma (,) and store it in a variable t1. 
+	#Add t1 to dic_numbers at the index int(t[0])
+
+
+num=1
+lang='urd'
+
+#print(str(num) + " in "+ lang +" : "+ str(numbersToText(num,lang)))
+#output: 1 in urd : Aik
+
+
