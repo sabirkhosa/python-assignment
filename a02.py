cc7005518530a4cb1f54a3e739a9a54f27048108
sabir balouch <p180085@nu.edu.pk>
2021-09-29 23:31:28 +0500

    Add files via upload

--- a02.py
+++ a02.py
@@ -1,33 +1,19 @@
-## IMPORTS GO HERE
-from scipy.constants import c
+## IMPORTS GO HERE if required
+
 ## END OF IMPORTS 
 
 
-#### YOUR CODE FOR calculate_force GOES HERE ####
-
-def calculate_force(m,a):
-    if type(a)==float and type(m)==float :
-        f=m*a
-        return (f)
+#### YOUR CODE FOR jobOffer GOES HERE ####
+def jobOffer (salary,location="peshswar"):
+    if location=="Peshawar" and salary>40000:
+        return "I’ll take it!"
+    elif location=="Karachi" and salary<100000:
+        return "No way"
+    elif salary>=60000:
+        return "I’ll take it!"
     else:
-        return None
+        return "No thanks, I can find something better."
+    
 #### End OF MARKER
 
-
-#### YOUR CODE FOR find_and_print_energy GOES HERE ####
-def find_print_energy(m):
-    if type(m)==float:
-	    e=m*pow(c,2)
-	    print('The energy equivalent of mass',m,'is:',e)
-	    return (e)
-    else:
-        return None
-#### End OF MARKER  
-
-
-
-
-if __name__ == '__main__': 
-    print(calculate_force(1.0, 0.2))
-    print(find_and_print_energy(0.0009))  
 
--- test_student.py
+++ test_student.py
@@ -1,63 +1,20 @@
+try: 
+    from a02 import jobOffer
+except: 
+    pass 
 
-
-import os
-
-try:
-    from a05b import writeMissingEntries
-
-except ImportError:
-    pass
-
-
-def test_missingEntries1():
-    try:
-        os.remove('logfile.log')
-        writeMissingEntries('18P-6089,Muhammad,1,4')
-        assert(os.path.isfile('logfile.log')==True)
-    except:
-        pass
-
-def test_missingEntries2():
-    try:
-        os.remove('logfile.log')
-        writeMissingEntries('18P-6089,Muhammad,1,2,4')
-        assert(os.path.isfile('logfile.log')==False)
-    except:
-        pass
-
-def test_missingEntries3():
-    data=[
-	'18P-0006,Ahmed Khan,1,8',
-	'18P-0130,Ali Hassan,2,2,4',
-	'18P-6061,Ikram Durrani,3,2,4',
-	'18P-6089,Muhammad,14',
-	'18P-6154,Syed Shahid Khaqani,1,2,3',
-	'18P-6064,Hamza Arif,6.5,7,2.5',
-	'18P-6065,Muhammad Ali,2,3.2,4'  
-	]
-
-    for i in range(len(data)):
-        writeMissingEntries(data[i]) 
-    with open('logfile.log','r') as flog:
-        assert(flog.readline()=='[18P-0006,Ahmed Khan,1,8]\n')
-        assert(flog.readline()=='[18P-6089,Muhammad,14]\n')
-
-def test_missingEntries4():
-    os.remove('logfile.log')
-    data=[
-	'18P-6089,Muhammad,14',
-	'18P-0006,Ahmed Khan,1,8',
-	'18P-0130,Ali Hassan,2,2,4',
-	'18P-6061,Ikram Durrani,3,2,4',
-	'18P-6154,Syed Shahid Khaqani,1,2,3',
-	'18P-6064,Hamza Arif,6.5,7,2.5',
-	'18P-6065,Muhammad Ali,2,3.2,4'  
-	]
-
-    for i in range(len(data)):
-        writeMissingEntries(data[i]) 
-    with open('logfile.log','r') as flog:
-        assert(flog.readline()=='[18P-6089,Muhammad,14]\n')
-        assert(flog.readline()=='[18P-0006,Ahmed Khan,1,8]\n')
-
+def test_jobOffer():
+    assert jobOffer(50000, "Peshawar")=="I’ll take it!"
+def test_jobOffer2():
+    assert jobOffer(40000, "Peshawar")=="No thanks, I can find something better."
+def test_jobOffer3():
+    assert jobOffer(50000, "Karachi")=="No way"
+def test_jobOffer4():
+    assert jobOffer(100000, "Karachi")=="I’ll take it!"
+def test_jobOffer5():
+    assert jobOffer(100000)=="I’ll take it!"
+def test_jobOffer6():
+    assert jobOffer(100000, 'Lahore')=="I’ll take it!"
+def test_jobOffer7():
+    assert jobOffer(4000,'Lahore')=="No thanks, I can find something better."
 
