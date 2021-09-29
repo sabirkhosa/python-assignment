07c720029fac33ddcb905db347b9593184921856
sabir balouch <p180085@nu.edu.pk>
2021-09-29 23:33:40 +0500

    Add files via upload

--- test_student.py
+++ test_student.py
@@ -1,20 +1,105 @@
-try: 
-    from a02 import jobOffer
-except: 
-    pass 
+try:
+    from a06 import get_student_marks
+except ImportError:
+    print("Bad import of get_student_marks")
 
-def test_jobOffer():
-    assert jobOffer(50000, "Peshawar")=="I’ll take it!"
-def test_jobOffer2():
-    assert jobOffer(40000, "Peshawar")=="No thanks, I can find something better."
-def test_jobOffer3():
-    assert jobOffer(50000, "Karachi")=="No way"
-def test_jobOffer4():
-    assert jobOffer(100000, "Karachi")=="I’ll take it!"
-def test_jobOffer5():
-    assert jobOffer(100000)=="I’ll take it!"
-def test_jobOffer6():
-    assert jobOffer(100000, 'Lahore')=="I’ll take it!"
-def test_jobOffer7():
-    assert jobOffer(4000,'Lahore')=="No thanks, I can find something better."
+try:
+    from a06 import get_grade
+except ImportError:
+    print("Bad import of get_grade")
 
+
+
+epsilon = 1e-4
+
+def test_marks_s1():
+    # grade_boundaries = {'A': 80, 'B': 70, 'C': 60, 'D': 50}
+    student_data = [ 
+        {'roll_no': 'p18-1001', 'marks': {
+                'english': (1.4, 2.5, 15, 9.6, 33), 
+                'calculus': (2.4, 1.5, 12, 1.6, 21),
+            }, 'attendance': 88.4
+        }, 
+        {'roll_no': 'p18-1002', 'marks': {
+                'english': (2.4, 1.5, 12, 1.6, 21),
+                'programming fundaments': (2.4, 1.5, 12, 1.6, 21),
+            }, 'attendance': 79.4
+        }, 
+        {'roll_no': 'p18-1003', 'marks': {
+                'calculus': (2.4, 1.5, 12, None, 21), 
+                'programming fundamentals': (2.4, 1.5, 12, 1.6, 21), 
+            }, 'attendance': 79.4
+        }, 
+    ]
+
+
+    student_marks = get_student_marks(student_data)
+    assert abs(student_marks['p18-1001']['english'] - 61.5) < epsilon
+    assert abs(student_marks['p18-1001']['calculus'] - 38.5) < epsilon
+
+    assert abs(student_marks['p18-1002']['english'] - 38.5) < epsilon
+    assert abs(student_marks['p18-1002']['programming fundaments'] - 38.5) < epsilon
+
+    assert abs(student_marks['p18-1003']['calculus'] - 36.9) < epsilon
+    assert abs(student_marks['p18-1003']['programming fundamentals'] - 38.5) < epsilon
+
+
+    # ensure no extra keys 
+    diff = list(set(student_marks['p18-1001'].keys()) - set(['english', 'calculus']))
+    assert len(diff) == 0, "Extra subjects found"
+
+
+
+def test_marks_s2():
+    # grade_boundaries = {'A': 80, 'B': 70, 'C': 60, 'D': 50}
+    student_data = [ 
+        {'roll_no': 'p18-1001', 'marks': {
+                'english': (1.4, 2.5, 15, 9.6, 33), 
+                'calculus': (2.4, 1.5, 12, 1.6, 21),
+            }, 'attendance': 88.4
+        }, 
+        {'roll_no': 'p18-1002', 'marks': {
+                'english': (2.4, 1.5, 12, 1.6, 21),
+                'programming fundaments': (2.4, 1.5, 12, 1.6, 21),
+            }, 'attendance': 79.4
+        }, 
+        {'roll_no': 'p18-1122', 'marks': {
+                'calculus': (2.4, 1.5, 12, 28, 21), 
+                'programming fundamentals': (2.4, 1.5, 12, 1.6, 21), 
+            }, 'attendance': 79.4
+        }, 
+    ]
+
+
+    student_marks = get_student_marks(student_data)
+    assert abs(student_marks['p18-1001']['english'] - 61.5) < epsilon
+    assert abs(student_marks['p18-1001']['calculus'] - 38.5) < epsilon
+
+    assert abs(student_marks['p18-1002']['english'] - 38.5) < epsilon
+    assert abs(student_marks['p18-1002']['programming fundaments'] - 38.5) < epsilon
+
+    assert abs(student_marks['p18-1122']['calculus'] - 64.9) < epsilon
+    assert abs(student_marks['p18-1122']['programming fundamentals'] - 38.5) < epsilon
+
+    # ensure no extra keys 
+    diff = list(set(student_marks['p18-1122'].keys()) - set(['programming fundamentals', 'calculus']))
+    assert len(diff) == 0, "Extra subjects found"
+
+
+
+def test_grades_s1():
+    grade_boundaries = {'A': 80, 'B': 70, 'C': 60, 'D': 50}
+    assert get_grade(65, grade_boundaries) == 'C'
+
+
+def test_grades_s2():
+    grade_boundaries = {'A': 70, 'B': 60, 'C': 50, 'D': 40}
+    assert get_grade(65, grade_boundaries) == 'B'
+
+def test_grades_s3():
+    grade_boundaries = {'A': 70, 'B': 60, 'C': 50, 'D': 40}
+    assert get_grade(5.7, grade_boundaries) == 'F'
+
+def test_grades_s4():
+    grade_boundaries = {'A': 90, 'B': 89, 'C': 88, 'D': 87}
+    assert get_grade(76, grade_boundaries) == 'F'
